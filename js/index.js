3dba78abd3176e51f724e64e9dc567d316695072
Matheus Vrech <whoismath@gmail.com>
2018-08-25 00:26:42 -0300

    adding cookie control

--- js/index.js
+++ js/index.js
@@ -1,21 +1,25 @@
 $(function(){
 
-	if ($.cookie("arduino"))
+	if (Cookies.get('arduino'))
 		$("#page-wrapper > main > div > div.container-fluid > div:nth-child(1)").hide();
 
 	$("#connect").click(function() {
 		if ($("#inputAddress").val() == "")
-			$("#inputAddress").val() = $("#inputAddress").attr("placeholder");
+			$("#inputAddress").val($("#inputAddress").attr("placeholder")); 
 
-			console.log("oi");
 		$.ajax({
 			url: "http://" + $("#inputAddress").val() + "/",
 			method: "get",
-			success: users => {
-				$.cookie("arduino", $("#inputAddress").val(), { expires : 1 });
-				console.log("o servidor está funcionando!");
-				$("#page-wrapper > main > div > div.container-fluid > div:nth-child(1)").hide();
-			},
+			dataType: 'jsonp',
+			crossdomain: true,
+			statusCode: {
+				200: function() {
+					Cookies.set('arduino', $("#inputAddress").val(), { expires: 7 });
+					console.log("o servidor está funcionando!");
+					$("#page-wrapper > main > div > div.container-fluid > div:nth-child(1)").hide();
+
+				}
+			}
 		  });
 
 	});
